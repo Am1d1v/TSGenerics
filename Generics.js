1521c09dddadcd663f132d58b2ce65584f1c42dc
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-21 15:13:48 +0300

    Add files via upload
    toString Function

--- Generics.js
+++ Generics.js
@@ -27,3 +27,26 @@ function getSplitedHalf(data) {
 }
 const arrRes = getSplitedHalf([1, 2, 3, 4]);
 console.log(arrRes);
+// toString Function
+function toString(data) {
+    if (Array.isArray(data)) {
+        return data.toString();
+    }
+    switch (typeof data) {
+        case 'string':
+            return data;
+        case 'number':
+        case 'symbol':
+        case 'bigint':
+        case 'boolean':
+            return data.toString();
+        case 'object':
+            return JSON.stringify(data);
+        default:
+            return undefined;
+    }
+}
+console.log(toString(3));
+console.log(toString(true));
+console.log(toString([1, 2, 3]));
+console.log(toString({ a: 1 }));
